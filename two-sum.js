6e9b25c7138899cb13505bc3b83737be59a7f679
Rachel (Xinyi) Wang <88614537+xinyirachel@users.noreply.github.com>
2023-02-27 19:00:48 +1100

    update

--- two-sum.js
+++ two-sum.js
@@ -4,24 +4,21 @@
 //give a array of numbers
 //return index of 2 numbers
 //2 number added up to target
+// https://www.youtube.com/watch?v=J0U1obg5BEM
 
+function addition(a, b) {
+	return a + b;
+};
 
-
-let twoSum = function twoSum(nums, target){
+function twoSum(nums, target){
   for (let i = nums.length -1 ; i >= 0; i--){
     for (let j = 0; j < i; j++){
-      if ( addition(nums[i]+nums[j]) === target){
+      if ( (nums[i]+nums[j]) === target){
         return [i,j];
       }
     }
   }
-}
-
-let addition = function(a, b) {
-	return a + b;
 };
 
-
-
-
-console.log(twoSum([2,7,5,6,7,7],11));
+const nums1 = [2,7,11,15]
+console.log(twoSum(nums1,9));
